8238ffb76b48af249522e6bc64bd6c6b3f5bf26e
young <gustmdehdud@gmail.com>
2025-09-27 16:06:40 +0900

    설명 추가

--- doyoung/week1/1681.py
+++ doyoung/week1/1681.py
@@ -4,13 +4,14 @@ N = int(value[0])
 L = value[1]
 
 
-count = 0
-last_label = 0
-label = 0
+count = 0 #라벨을 만든 횟수
+last_label = 0  #최종 라벨 번호
+label = 0 #라벨 번호
 
 while count < N:
      label += 1
 
+     # 순회하는 도중에 L값이 포함되어 있지 않으면 라벨 추가
      if(str(label).find(L) < 0):
         count += 1
         last_label = label
