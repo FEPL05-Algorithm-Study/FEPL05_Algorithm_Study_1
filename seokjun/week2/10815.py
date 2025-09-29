caedf6f34a1a5840d5b826e8d41c1d510268f563
aiden0413 <aiden0413@naver.com>
2025-09-29 18:50:08 +0900

    수정

--- seokjun/week2/10815.py
+++ seokjun/week2/10815.py
@@ -3,12 +3,9 @@ cards = list(map(int,input().split(' ')))
 m = int(input())
 nums = list(map(int,input().split(' ')))
 
-s = set()
-for i in range(n):
-    s.add(cards[i])
-
-answer = [0]*m
+s = set(cards)
+answer = []
 for i in range(m):
-    answer[i] = int(nums[i] in s)
+    answer.append(int(nums[i] in s))
 
 print(' '.join(map(str,answer)))
