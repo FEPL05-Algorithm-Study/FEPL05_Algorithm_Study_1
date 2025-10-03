994cea57e19e880cf7391e4885c52f6c3969109b
aiden0413 <aiden0413@naver.com>
2025-10-04 03:12:50 +0900

    수정

--- seokjun/week3/1388.py
+++ seokjun/week3/1388.py
@@ -6,9 +6,6 @@ board = [[x for x in input()] for _ in range(n)]
 
 visited = [[0]*m for _ in range(n)]
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
 def dfs(x,y,shape):
     # 범위 밖이거나 모양이 다르면 종료
     if x<0 or x>=n or y<0 or y>=m or visited[x][y] or board[x][y]!=shape:
@@ -17,10 +14,9 @@ def dfs(x,y,shape):
     visited[x][y] = 1
 
     # '-' 모양이면 동,서만 탐색 '|'모양이면 남,북만 탐색
-    for i in range(2):
-        i = i if shape == '-' else i+2
-        nx,ny = x+dx[i],y+dy[i]
-        dfs(nx,ny,shape)
+    dx,dy = [0,1] if shape == '-' else [1,0]
+    nx,ny = x+dx,y+dy
+    dfs(nx,ny,shape)
 
 answer = 0
 
