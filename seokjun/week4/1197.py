31c7ff578845b4b3538a3b87ddb7b0338f2e327d
aiden0413 <aiden0413@naver.com>
2025-10-12 21:58:53 +0900

    수정

--- seokjun/week4/1197.py
+++ seokjun/week4/1197.py
@@ -8,7 +8,7 @@ edges = [list(map(int, input().split())) for _ in range(e)]
 # 간선을 가중치가 작은 순으로 정렬
 edges.sort(key=lambda x:x[2])
 
-# 유니온 파인드를 사용해 가중치가 가장 작은간선부터 추가해 나감
+# 유니온 파인드를 사용해 가중치가 가장 작은간선의 양 끝 정점을 추가해 나감
 parents = [x for x in range(v+1)]
 level = [0] * (v+1)
 
@@ -29,14 +29,17 @@ def union(a,b):
         level[a] += 1
         parents[b] = a
 
-answer = 0
+mst_edges = []
 
 for a,b,w in edges:
-    # 가중치가 가장 작은 간선부터 간선추가
+    # 간선의 개수가 v-1이면 종료
+    if len(mst_edges) == v-1:
+        break
+
+    # 가중치가 가장 작은 간선부터 mst에 포함
     # 간선이 추가된다면 a와 b를 같은 집합에 추가
-    # 만약 모든 노드가 연결되게 되면 find(a)==find(b) 가 됨
     if find(a) != find(b):
-        answer += w
+        mst_edges.append(w)
         union(a,b)
 
-print(answer)
+print(sum(mst_edges))
