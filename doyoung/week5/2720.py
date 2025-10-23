20368e67aadcd18b86b46851f95e926042e496d6
young <gustmdehdud@gmail.com>
2025-10-24 03:54:21 +0900

    설명 주석 추가

--- doyoung/week5/2720.py
+++ doyoung/week5/2720.py
@@ -4,17 +4,17 @@ input = sys.stdin.readline
 
 T = int(input())
 
-cent = [25, 10, 5, 1]
+cent = [25, 10, 5, 1] # 입력값이 센트이므로 동일하게 변경 
 
 for i in range(T):
     change = []
-    c = int(input())
+    c = int(input()) # 입력값
 
     for j in cent:
-        change.append(str(c // j))
-        c %= j
+        change.append(str(c // j)) # 동전개수 추가
+        c %= j # 거슬러주고 남은 금액 입력
 
-    print(' '.join(change))
+    print(' '.join(change)) # join으로 출력
 
     
     
